19643b6cc829387e9c55403bcedf162ca75152ce
Abdul Rahman <abdul.rahman@klikdokter.com>
2024-09-12 20:59:15 +0700

    feat(seo): change meta title and icon

--- src/app/weather/layout.tsx
+++ src/app/weather/layout.tsx
@@ -1,7 +1,7 @@
-import type {Metadata} from 'next'
-import { ReactNode } from 'react'
+import type { Metadata } from 'next';
+import { ReactNode } from 'react';
 
-import WeatherProvider from '@/components/WeatherProvider'
+import WeatherProvider from '@/components/WeatherProvider';
 
 /**
  * Default metadata.
@@ -9,19 +9,19 @@ import WeatherProvider from '@/components/WeatherProvider'
  * @see https://nextjs.org/docs/app/api-reference/file-conventions/metadata
  */
 export const metadata: Metadata = {
-  title: 'test meta title',
-  description: 'test description'
-}
+  title: 'Weather App',
+  description: 'Weather App forecast Indonesia areas',
+};
 
 /**
  * The weather (client) root layout.
  *
  * @see https://nextjs.org/docs/app/api-reference/file-conventions/layout
  */
-export default function WeatherLayout({children}: { children: ReactNode }) {
+export default function WeatherLayout({ children }: { children: ReactNode }) {
   return (
     <WeatherProvider>
       <section>{children}</section>
     </WeatherProvider>
-  )
+  );
 }

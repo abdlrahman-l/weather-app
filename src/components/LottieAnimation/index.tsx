5b1b94188d900b605d9ab7bc98c2b25e52f350d2
abdlrahman-l <abdulrahmaninformatics@gmail.com>
2024-12-03 11:32:02 +0700

    fix(weather): play animation when expanded

--- src/components/LottieAnimation/index.tsx
+++ src/components/LottieAnimation/index.tsx
@@ -1,43 +1,65 @@
 'use client';
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import React, { Dispatch, Ref, useEffect, useState } from 'react';
+import { useLottie } from 'lottie-react';
+import React, { Ref, useEffect } from 'react';
 
 import Skeleton from '../Skeleton';
 
 type LottieAnimationProps = {
-    importAnimation: (cb: Dispatch<any>) => void;
-    lottieProps?: Record<string, any>;
-    onLoaded?: () => void;
-    lottieRef?: Ref<any>; 
+  importAnimation: (cb: (animationData: any) => void) => void;
+  lottieProps?: Record<string, any>;
+  onLoaded?: () => void;
+  lottieRef?: Ref<any>;
+  playAnimation?: boolean;
 };
-const LottieAnimation = ({ importAnimation, lottieProps = {}, onLoaded, lottieRef }: LottieAnimationProps) => {
-    const [animationData, setAnimationData] = useState<any>();
-    const [Lottie, setLottie] = useState<any>();
-    
-    useEffect(() => {
-        importAnimation(setAnimationData);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
 
-    useEffect(() => {
-        import('lottie-react/build/index').then((l) => setLottie(l));
-    }, []);
+const LottieAnimation = ({
+  importAnimation,
+  lottieProps = {},
+  onLoaded,
+  lottieRef,
+  playAnimation,
+}: LottieAnimationProps) => {
+  const [animationData, setAnimationData] = React.useState<any>(null);
 
-    useEffect(() => {
-        if (Lottie && animationData) {
-            onLoaded?.();
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [Lottie, animationData]);
+  useEffect(() => {
+    importAnimation(setAnimationData);
+  }, [importAnimation]);
 
-    if (!Lottie || !animationData) return (
-        <Skeleton style={{
-            height: lottieProps?.style?.height,
-            width: lottieProps?.style?.height
-        }} className='rounded-lg'/>
-    )
-    
-    return <Lottie.default ref={lottieRef} animationData={animationData} {...lottieProps} />;
+  const options = {
+    animationData,
+    ...lottieProps,
+    lottieRef,
+  };
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const { View, play } = useLottie(options);
+
+  if (playAnimation) {
+    play();
+  }
+
+  useEffect(() => {
+    if (animationData) {
+      onLoaded?.();
+    }
+  }, [animationData, onLoaded]);
+
+  if (!animationData) {
+    return (
+      <Skeleton
+        style={{
+          height: lottieProps?.style?.height,
+          width: lottieProps?.style?.height,
+        }}
+        className='rounded-lg'
+      />
+    );
+  }
+
+  return View;
 };
+
 export default LottieAnimation;
--- src/features/weather/components/VerticalWeather/index.tsx
+++ src/features/weather/components/VerticalWeather/index.tsx
@@ -38,6 +38,7 @@ const VerticalWeather = ({
         },
         autoplay: false,
       }}
+      playAnimation={isExpanded}
     />
   );
 
